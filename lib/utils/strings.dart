6ad990b7a0009c27a292dd99a585ef38e52d9590
anchliyatushar <anchliyatushar@gmail.com>
2020-10-09 16:40:07 +0530

    basic bloc observer setup and added sting utils

---
+++ lib/utils/strings.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+// color consts
+const Color light_grey = Color(0xff272c2f);
+const Color my_black = Color(0xff313639);
+const String my_font = 'GoogleSans';
