b3a1e52e6f556757a26e958a726b99f590d4de24
anchliyatushar <anchliyatushar@gmail.com>
2020-10-09 17:06:01 +0530

    added firebase

--- lib/routes/router.dart
+++ lib/routes/router.dart
@@ -1,6 +1,9 @@
 import 'package:auto_route/auto_route_annotations.dart';
 import 'package:stack_finance_assignment/pages/stack_finance.dart';
 
+// use this command to generate routes and to watch - flutter packages pub run build_runner watch
+// use this command to generate routes only - flutter packages pub run build_runner
+
 @autoRouter
 class $Router {
   @initial
