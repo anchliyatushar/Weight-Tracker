ed43422fa040b8a16e9bca64074219b4b675b100
anchliyatushar <anchliyatushar@gmail.com>
2020-10-09 16:41:26 +0530

    added global bloc provider for our app

---
+++ lib/bloc_provider.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:stack_finance_assignment/app.dart';
+import 'package:stack_finance_assignment/cubits/login_cubit/login_cubit.dart';
+import 'package:stack_finance_assignment/repositories/login_repository.dart';
+
+class BlocProviderScreen extends StatefulWidget {
+  @override
+  _BlocProviderScreenState createState() => _BlocProviderScreenState();
+}
+
+class _BlocProviderScreenState extends State<BlocProviderScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(providers: [
+      BlocProvider(
+        create: (context) => LoginCubit(
+          LoginRepository(),
+        ),
+      ),
+
+      // BlocProvider(
+      //   create: (context) => NavigatorCubit(),
+      // ),
+    ], child: App());
+  }
+}
