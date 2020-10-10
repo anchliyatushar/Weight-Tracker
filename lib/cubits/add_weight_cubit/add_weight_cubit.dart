6e62869dad7d55acd060c1340e090c2ab9b2df27
anchliyatushar <anchliyatushar@gmail.com>
2020-10-10 06:24:26 +0530

    add weight cubit updated

--- lib/cubits/add_weight_cubit/add_weight_cubit.dart
+++ lib/cubits/add_weight_cubit/add_weight_cubit.dart
@@ -1,11 +1,36 @@
 import 'package:bloc/bloc.dart';
-import 'package:equatable/equatable.dart';
+import 'package:stack_finance_assignment/cubits/user_cubit/user_cubit.dart';
+import 'package:stack_finance_assignment/models/weight_model.dart';
 import 'package:stack_finance_assignment/repositories/weight_repository.dart';
 
-part 'add_weight_state.dart';
-
-class AddWeightCubit extends Cubit<AddWeightState> {
+class AddWeightCubit extends Cubit<WeightModel> {
   final WeightRepository weightRepository;
+  final WeightModel weightModel;
 
-  AddWeightCubit(this.weightRepository) : super(AddWeightInitial());
+  AddWeightCubit(this.weightRepository, this.weightModel) : super(weightModel);
+
+  emitNewHeight(WeightModel weightModel, double height) {
+    emit(weightModel.copyWith(
+        height: double.parse(height.toStringAsExponential(1))));
+  }
+
+  emitNewWeight(WeightModel weightModel, double weight) {
+    emit(weightModel.copyWith(
+        weight: double.parse(weight.toStringAsExponential(1))));
+  }
+
+  emitNewTimeStamp(WeightModel weightModel, DateTime dateTime) {
+    emit(weightModel.copyWith(selectedDate: dateTime));
+  }
+
+  sendDataToDb(WeightModel weightModel, String uid) {
+    weightRepository.addWeightToDb(uid, weightModel).then((value) {
+      emit(weightModel.copyWith(id: "", status: WeightStatus.Success));
+      emit(weightModel.copyWith(id: "", status: WeightStatus.Initial));
+    }).catchError((err) {
+      print(err);
+      emit(weightModel.copyWith(status: WeightStatus.Error));
+      emit(weightModel.copyWith(status: WeightStatus.Initial));
+    });
+  }
 }
