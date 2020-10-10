726e13d389b091867c0db530ead9624bbe187f09
anchliyatushar <anchliyatushar@gmail.com>
2020-10-10 06:21:05 +0530

    bloc provider updated with add and edit cubits

--- lib/bloc_provider.dart
+++ lib/bloc_provider.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:stack_finance_assignment/app.dart';
 import 'package:stack_finance_assignment/cubits/add_weight_cubit/add_weight_cubit.dart';
+import 'package:stack_finance_assignment/cubits/home_cubit/home_cubit.dart';
 import 'package:stack_finance_assignment/cubits/login_cubit/login_cubit.dart';
 import 'package:stack_finance_assignment/cubits/user_cubit/user_cubit.dart';
+import 'package:stack_finance_assignment/models/weight_model.dart';
 import 'package:stack_finance_assignment/repositories/login_repository.dart';
 import 'package:stack_finance_assignment/repositories/weight_repository.dart';
 
@@ -25,10 +27,21 @@ class _BlocProviderScreenState extends State<BlocProviderScreen> {
           BlocProvider.of<UserCubit>(context),
         ),
       ),
-      BlocProvider(create: (context) => AddWeightCubit(WeightRepository())),
-      // BlocProvider(
-      //   create: (context) => NavigatorCubit(),
-      // ),
+      BlocProvider(
+        create: (context) => HomeCubit(
+          WeightRepository(),
+        ),
+      ),
+      BlocProvider(
+          create: (context) => AddWeightCubit(
+              WeightRepository(),
+              WeightModel(
+                  height: 5,
+                  weight: 50,
+                  selectedDate: DateTime.now(),
+                  timestamp: DateTime.now().toString(),
+                  id: "",
+                  status: WeightStatus.Initial))),
     ], child: App());
   }
 }
